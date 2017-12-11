18e92237a7217325c9442dba438a635b44d66849
Anywhereinfo <batth.maninder@gmail.com>
2017-12-11 07:07:03 -0500

    adding structured arguments

--- src/main/java/com/ideasforsharing/demo/controllers/SleuthController.java
+++ src/main/java/com/ideasforsharing/demo/controllers/SleuthController.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import static net.logstash.logback.argument.StructuredArguments.*;
 
 @RestController
 public class SleuthController {
@@ -16,12 +16,17 @@ public class SleuthController {
 	@GetMapping("/")
 	public String  helloSleuth() {
 		int x = (int) Math.floor(new Random().nextInt(10));
+		log.info("log message {}", value("name", "value"));
 		if (x%2 ==0)
 		{
 			log.info("Hello Sleuth");
-			return "success";
+			
 		} else
-			throw new RuntimeException();
+		{
+			log.error("{}", keyValue("stack_trace", new RuntimeException().getStackTrace()));
+			log.error("", new RuntimeException());
+		}
+		return "success: " + x;
 		
 	}
 }
